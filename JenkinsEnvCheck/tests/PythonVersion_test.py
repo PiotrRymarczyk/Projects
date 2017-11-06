b68172e701df65090c97c7a3b32992bb5beb884e
PiotrRymarczyk <rymarczyk@wp.pl>
2017-11-06 22:24:06 +0100

    test for python version added

---
+++ JenkinsEnvCheck/tests/PythonVersion_test.py
@@ -0,0 +1,31 @@
+'''
+Created on 2017/11/06
+Modified on 2017/11/06
+
+@author: Piotr Rymarczyk
+
+UnitTests for Python Version
+
+'''
+
+import unittest
+import sys
+
+
+class Test_PythonVersion(unittest.TestCase):
+    '''
+    UnitTests for python's version
+    '''
+    
+    def test_check_if_3(self):
+        (result,b,c,d,e) = sys.version_info
+        message = 'Major version is {0}'.format(result)
+        self.assertListEqual(result, 3, message)
+    
+    def test_check_if_2(self):
+        (result,b,c,d,e) = sys.version_info
+        message = 'Major version is {0}'.format(result)
+        self.assertListEqual(result, 2, message)
+
+if __name__ == "__main__":
+    unittest.main()
