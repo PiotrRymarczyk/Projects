cfd8834845e66d524190d1a53ad1eda93de3382f
PiotrRymarczyk <rymarczyk@wp.pl>
2017-11-06 22:47:04 +0100

    test for python version fixed

--- JenkinsEnvCheck/tests/PythonVersion_test.py
+++ JenkinsEnvCheck/tests/PythonVersion_test.py
@@ -19,12 +19,12 @@ class Test_PythonVersion(unittest.TestCase):
     
     def test_check_if_3(self):
         (result,b,c,d,e) = sys.version_info
-        message = 'Major version is {0}'.format(result)
+        message = 'Major version is {0}, expected 3'.format(result)
         self.assertEqual(result, 3, message)
     
     def test_check_if_2(self):
         (result,b,c,d,e) = sys.version_info
-        message = 'Major version is {0}'.format(result)
+        message = 'Major version is {0}, expected 2'.format(result)
         self.assertEqual(result, 2, message)
 
 if __name__ == "__main__":
